2ff95817b9749b6a8a68f21c70c75e6429cf5ca4
Eduardo Machado <educamachado2006@gmail.com>
2024-03-20 21:13:01 +0100

    ground, player and collisions
    yeeaa

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        
+    }
+}
