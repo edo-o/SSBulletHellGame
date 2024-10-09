845bdbac281314969b2c4526544a2fdfc3a2e065
Eduardo Machado <educamachado2006@gmail.com>
2024-10-09 14:39:56 +0200

    Bullet spawner. Bullet hell patterns

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,30 +6,25 @@ public class Bullet : MonoBehaviour
 {
     public int damage = 1;
     public float bulletLife = 1f;
-    public float rotation = 0f;
     public float speed = 1f;
 
     private Vector2 spawnpoint;
     private float timer = 0f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
     void Start()
     {
-        spawnpoint = new Vector2(transform.position.x, transform.position.y);
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * speed;
     }
 
     void Update()
     {
         if(timer > bulletLife) Destroy(this.gameObject);
         timer += Time.deltaTime;
-        transform.position = Movement(timer);
     }
 
-    private Vector2 Movement(float timer)
-    {
-        float x = timer * speed * transform.right.x;
-        float y = timer * speed * transform.right.y;
-        return new Vector2(x+spawnpoint.x, y+spawnpoint.y);
-    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
