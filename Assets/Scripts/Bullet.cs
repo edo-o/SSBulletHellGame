ade4983fc167ebf07120ae4d2d3f53d9702f20c9
Eduardo Machado <educamachado2006@gmail.com>
2024-10-24 21:30:04 +0200

    Revert "ad"
    This reverts commit 10cbec5d8de6d5906b0e4baafbc2bf2875b5012e, reversing
    changes made to 62c4a82b1c249fd78bd092b42df28e35eca7001b.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,25 +6,30 @@ public class Bullet : MonoBehaviour
 {
     public int damage = 1;
     public float bulletLife = 1f;
+    public float rotation = 0f;
     public float speed = 1f;
 
     private Vector2 spawnpoint;
     private float timer = 0f;
-    public GameObject bulletPrefab;
-    public Transform firePoint;
 
     void Start()
     {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.velocity = transform.up * speed;
+        spawnpoint = new Vector2(transform.position.x, transform.position.y);
     }
 
     void Update()
     {
         if(timer > bulletLife) Destroy(this.gameObject);
         timer += Time.deltaTime;
+        transform.position = Movement(timer);
     }
 
+    private Vector2 Movement(float timer)
+    {
+        float x = timer * speed * transform.right.x;
+        float y = timer * speed * transform.right.y;
+        return new Vector2(x+spawnpoint.x, y+spawnpoint.y);
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
