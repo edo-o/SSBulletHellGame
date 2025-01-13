a70439e72a156f9460c909b66f51eb3ce608dbea
Eduardo Machado <educamachado2006@gmail.com>
2025-01-13 15:01:30 +0100

    Win/Lose condition (Not working)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // For reloading scenes
+using UnityEngine.UI; // For managing UI elements
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject winPanel;
+    public GameObject losePanel;
+    public HealthSystem playerHealth;
+    public HealthSystem bossHealth;
+
     public static GameManager instance;
     private bool levelWon = false;
+
+    private void Update()
+    {
+        if (bossHealth != null && bossHealth.currentHealth <= 0)
+        {
+            WinGame();
+        }
+        if (playerHealth != null && playerHealth.currentHealth <= 0)
+        {
+            LoseGame();
+        }
+    }
+
+    void WinGame()
+    {
+        winPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    void LoseGame()
+    {
+        losePanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void RetryGame()
+    {
+        Time.timeScale = 1; // Reset time scale
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+    }
+
     void Awake()
     {
         if (instance == null)
